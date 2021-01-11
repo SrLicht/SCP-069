dad72029a6fea00b248c24ceb50c368c9ec4f2a0
Franco <francoquiroz_13@live.com>
2021-01-11 14:10:33 -0300

    SL Update and Exiled Update

--- SCP-069/Scp069/SCP-069/CloneGuy.cs
+++ SCP-069/Scp069/SCP-069/CloneGuy.cs
@@ -72,7 +72,7 @@ namespace Scp069.SCP_069
                 enableDamage = Timing.RunCoroutine(EnableDamage(Plugin.Instance.Config.GracePeriodStart));
 
                 MainHandlers.cloneGuy = player;
-                player.CustomPlayerInfo = $"<color=#E7205C>{player.DisplayNickname}</color>\n<b><color=red>SCP-069</color></b>";
+                player.CustomInfo = $"<color=#E7205C>{player.DisplayNickname}</color>\n<b><color=red>SCP-069</color></b>";
 
                 player.Health = Plugin.Instance.Config.ClonerHealth;
                 player.MaxHealth = Plugin.Instance.Config.ClonerMaxHealth;
@@ -93,7 +93,7 @@ namespace Scp069.SCP_069
                 PlayerEvents.ChangingRole -= OnRoleChange;
                 PlayerEvents.Left -= OnLeave;
                 Scp049.StartingRecall -= OnRecall;
-                player.CustomPlayerInfo = $"";
+                player.CustomInfo = $"";
 
                 MainHandlers.cloneGuy = null;
 
@@ -125,7 +125,7 @@ namespace Scp069.SCP_069
 
                 if (ev.NewRole != RoleType.Scp049)
                 {
-                    ev.Player.CustomPlayerInfo = "";
+                    ev.Player.CustomInfo = "";
                     ev.Player.DisplayNickname = null;
                     Destroy(this);
                 }
@@ -159,7 +159,7 @@ namespace Scp069.SCP_069
                     return;
 
                 Destroy(this);
-                ev.Target.CustomPlayerInfo = "";
+                ev.Target.CustomInfo = "";
                 ev.Target.DisplayNickname = null;
             }
             catch (Exception e)
