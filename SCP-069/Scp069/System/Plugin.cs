8bfeba8dc5aa0867b2155f85f0396022702b4d39
Franco <francoquiroz_13@live.com>
2021-07-27 16:37:42 -0300

    Updating Exiled
    Also changed a couple Logs and removing some as well.

--- SCP-069/Scp069/System/Plugin.cs
+++ SCP-069/Scp069/System/Plugin.cs
@@ -11,15 +11,15 @@ namespace Scp069.System
         public static Plugin Instance;
 
         MainHandlers handler;
-        public override Version Version => new Version(1,0,0);
-        public override Version RequiredExiledVersion => new Version(2,8,0);
+        public override Version Version => new Version(1,5,1);
+        public override Version RequiredExiledVersion => new Version(2,11,0);
         public override string Author { get; } = "SrLicht & Beryl";
 
         public override void OnEnabled()
         {
             try
             {
-                Log.Debug("Initializing EventsHandler...");
+                Log.Info("Initializing EventsHandler...");
 
                 Instance = this;
 
@@ -27,14 +27,14 @@ namespace Scp069.System
 
                 Server.RoundEnded += handler.RoundEnd;
                 Server.RoundStarted += handler.RoundStart;
-                Player.Verified += handler.OnVerify;
+                //Player.Verified += handler.OnVerify;
 
                 #region Logo
 
                 if (!Config.NotLogo)
                 {
-                    
 
+                    // I still think it's better than the one Joker did in Exiled. Don't hit me Joker.
                     Log.Info("####################################################################");
                     Log.Info("");
                     Log.Debug(" ######   ######  ########            #####    #######   #######  ");
@@ -54,7 +54,7 @@ namespace Scp069.System
             }
             catch (Exception e)
             {
-                Log.Error("Problem loading plugin: " + e);
+                Log.Error($"{e.TargetSite} {e.Message}\n{e.StackTrace}");
             }
 
         }
@@ -64,7 +64,7 @@ namespace Scp069.System
 
             Server.RoundEnded -= handler.RoundEnd;
             Server.RoundStarted -= handler.RoundStart;
-            Player.Verified -= handler.OnVerify;
+            //Player.Verified -= handler.OnVerify;
             handler = null;
             Instance = null;
             base.OnDisabled();
