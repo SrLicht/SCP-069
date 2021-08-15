851f04b2c53098408d03fd4e5313e3dc6ef32d20
Franco <francoquiroz_13@live.com>
2021-08-15 14:47:28 -0300

    Fixing the SCP-069 tag when the component was destroyed it was not erased correctly

--- SCP-069/Scp069/Component/Scp069Component.cs
+++ SCP-069/Scp069/Component/Scp069Component.cs
@@ -46,6 +46,9 @@ namespace Scp069.Component
                 if (scp069 != null)
                 {
                     Handlers.MainHandler.scp069Players.Remove(scp069);
+                    scp069.CustomInfo = null;
+                    scp069.DisplayNickname = null;
+                    RemoveTagForVictims();
                 }
 
                 Destroy(this);
@@ -99,12 +102,20 @@ namespace Scp069.Component
             var nully = "player is null";
             Log.Debug($"Destroying {this} which was {(scp069 == null ? nully : scp069.Nickname)}", Plugin.Instance.Config.Debug);
         }
+
+        /// <summary>
+        /// The grace period when killing and starting the game is thanks to this.
+        /// </summary>
         private IEnumerator<float> EnableDamage(float seconds)
         {
             damageEnable = false;
             yield return Timing.WaitForSeconds(seconds);
             damageEnable = true;
         }
+
+        /// <summary>
+        /// This takes care of the damage to 069 and increases that damage more and more.
+        /// </summary>
         private void DoDamage()
         {
             if (damageEnable && damageTimer <= Time.time)
@@ -115,26 +126,42 @@ namespace Scp069.Component
             }
         }
 
+        /// <summary>
+        /// Preventing it from creating zombies
+        /// </summary>
+        /// <param name="ev"></param>
         private void OnTryToRevive(StartingRecallEventArgs ev)
         {
             if (ev.Scp049 != scp069) return;
             ev.IsAllowed = false;
         }
+
+        /// <summary>
+        /// Destroys the component when 069 dies
+        /// </summary>
+        /// <param name="ev"></param>
         private void OnDeath(DyingEventArgs ev)
         {
             if (ev.Target != scp069) return;
-
-            ev.Target.CustomInfo = null;
-            ev.Target.DisplayNickname = null;
             Destroy();
 
         }
+
+        /// <summary>
+        /// If 069 changes its role when it has the component, the component will be destroyed.
+        /// </summary>
+        /// <param name="ev"></param>
         private void OnChangingRole(ChangingRoleEventArgs ev)
         {
             if (ev.Player != scp069) return;
 
             Destroy();
         }
+
+        /// <summary>
+        /// Where the magic happens. The 069 recovers health, changes shape, obtains its victim's inventory and obtains movement speed (if this is activated).
+        /// </summary>
+        /// <param name="ev"></param>
         private void OnKill(DyingEventArgs ev)
         {
             if (ev.Killer != scp069 || ev.Target == scp069 || ev.Target == null || ev.Target.IsScp) return;
@@ -166,7 +193,7 @@ namespace Scp069.Component
                 ev.Target.ClearBroadcasts();
                 ev.Target.Broadcast(Plugin.Instance.Config.Broadcasting.BroadcastDuration, Plugin.Instance.Config.Broadcasting.Killbroadcast);
             }
-            // Steal shape 
+            // Steal shape and name.
             scp069roletype = ev.Target.Role;
             string targetname = string.IsNullOrEmpty(ev.Target.DisplayNickname) ? ev.Target.Nickname : ev.Target.DisplayNickname;
             if (!Handlers.MainHandler.victims.Contains(ev.Target))
@@ -174,7 +201,7 @@ namespace Scp069.Component
                 Handlers.MainHandler.victims.Add(ev.Target);
             }
             ev.Killer.DisplayNickname = targetname;
-            UpdateNickname(targetname);
+            UpdateNameForVictims(targetname);
             //UpdateItemOnHand(t); This not work for now.
             ev.Killer.Change069Appearance(scp069roletype);
             //Movement speed
@@ -185,6 +212,11 @@ namespace Scp069.Component
                 playerEffect.Intensity = Plugin.Instance.Config.Scp069.movementSpeedIntesify;
             }
         }
+
+        /// <summary>
+        /// Preventing damage from SCP-207
+        /// </summary>
+        /// <param name="ev"></param>
         private void OnHurting(HurtingEventArgs ev)
         {
             if (ev.Target == scp069 && ev.HitInformations.GetDamageType() == DamageTypes.Scp207)
@@ -192,6 +224,10 @@ namespace Scp069.Component
                 ev.Amount = 0;
             }
         }
+
+        /// <summary>
+        /// If the plugin configuration says that 069 does not have to leave bodies, this will do exactly that.
+        /// </summary>
         private void OnSpawnRagdoll(SpawningRagdollEventArgs ev)
         {
             if (ev.Killer != scp069) return;
@@ -201,16 +237,27 @@ namespace Scp069.Component
             }
         }
 
+        /// <summary>
+        /// Each time a player joins the server, the 069 appearance will be updated for all players, including the new player. This is to avoid problems with players not seeing the current 069 appearance or seeing an old one.
+        /// </summary>
         private void OnPlayerVerify(VerifiedEventArgs ev)
         {
             scp069.Change069Appearance(scp069roletype);
         }
+
+        /// <summary>
+        /// If 069 leaves the server, the component will be destroyed.
+        /// </summary>
         private void OnPlayerLeft(DestroyingEventArgs ev)
         {
             if (ev.Player != scp069) return;
             Destroy();
         }
-        private void UpdateNickname(string nicktodisplay)
+
+        /// <summary>
+        /// Updates the name that 069 victims see upon death.
+        /// </summary>
+        private void UpdateNameForVictims(string nicktodisplay)
         {
             try
             {
@@ -225,6 +272,30 @@ namespace Scp069.Component
             }
 
         }
+
+        /// <summary>
+        /// Removes the (SCP-069) tag from its victims when the component is destroyed.
+        /// </summary>
+        private void RemoveTagForVictims()
+        {
+            try
+            {
+                foreach (Player player in Handlers.MainHandler.victims)
+                {
+                    Timing.CallDelayed(0.5f, () => player.SendFakeSyncVar(scp069.ReferenceHub.networkIdentity, typeof(NicknameSync), nameof(NicknameSync.Network_displayName), $"{scp069.Nickname}"));
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error($"{e.TargetSite} {e.Message}\n{e.StackTrace}");
+            }
+
+        }
+
+        /// <summary>
+        /// This is supposed to make the 069 hold the object the victim had when he died, it stopped working, no idea why. I'm not good at this Mirror stuff
+        /// </summary>
+        /// <param name="it"></param>
         private void UpdateItemOnHand(ItemType it)
         {
             try
@@ -232,7 +303,7 @@ namespace Scp069.Component
                 foreach (Player player in Player.List)
                 {
                     player.SendFakeSyncVar(scp069.ReferenceHub.networkIdentity, typeof(Inventory),
-                  nameof(Inventory._curItemSynced), (sbyte)it);
+                  nameof(Inventory.Network_curItemSynced), (sbyte)it);
                 }
             }
             catch (Exception e)
