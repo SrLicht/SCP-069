7e1b8a571d7b8d3c076788e746eda51384d97aca
SrLicht <francoquiroz_13@live.com>
2022-05-06 21:09:15 -0300

    Update MainHandler.cs

--- SCP-069/Scp069/Handlers/MainHandler.cs
+++ SCP-069/Scp069/Handlers/MainHandler.cs
@@ -2,7 +2,10 @@
 using Exiled.CustomRoles.API;
 using Exiled.CustomRoles.API.Features;
 using Exiled.Events.EventArgs;
+using MEC;
 using Scp069.System;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Scp069.Handlers
@@ -26,9 +29,9 @@ namespace Scp069.Handlers
                 if (Player.List.Count() < Plugin.Config.PlayersNeededToSpawn)
                     return;
 
-                if (Plugin.random.Next(1, 100) <= Plugin.Config.Scp069Chance)
+                if(Plugin.random.Next(1, 100) <= Plugin.Config.Scp069Chance)
                 {
-                    if (CustomRole.TryGet(69, out var scp069))
+                    if(CustomRole.TryGet(69, out var scp069))
                     {
                         scp069.AddRole(ev.Player);
                     }
